299fb65905682dfcd41e6cbe80f1de6b25e794e5
Tim Cheung <tim.cheung@circleci.com>
2024-04-09 21:05:47 -0400

    test

--- cypress/e2e/admin-app/ui-tests/addCustomer.cy.ts
+++ cypress/e2e/admin-app/ui-tests/addCustomer.cy.ts
@@ -1,8 +1,8 @@
 describe('Add Customer via PetShop-AdminApp', () => {
     beforeEach(() => {
         cy.navigateTo('/login');
-        cy.fixture('locators.json').then((locators) => { 
-            cy.loginAdminUI(locators.adminLoginField, locators.adminLoginBtn, 
+        cy.fixture('locators.json').then((locators) => {
+            cy.loginAdminUI(locators.adminLoginField, locators.adminLoginBtn,
                 Cypress.env('adminEmail'), Cypress.env('adminPassword')
             );
         });
@@ -11,10 +11,10 @@ describe('Add Customer via PetShop-AdminApp', () => {
 
     it('should allow to add a new customer', () => {
         cy.contains('Customers').click();
-        cy.contains(' add new customer ',{timeout: 6000}).click();
+        cy.contains(' gibberish ',{timeout: 6000}).click();
         cy.generateRandomUser().then((randomUser) => {
             const user = randomUser;
-            cy.fixture('locators.json').then((locators) => { 
+            cy.fixture('locators.json').then((locators) => {
                 cy.addCustomer(locators.adminNewCustomerFields, locators.adminAddCustomerBtn, user);
             });
         });
